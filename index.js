43d7a98a448c679e3e40be26fc3982c83292127d
Xavier Lange <xrlange@gmail.com>
2015-10-21 15:00:37 -0700

    Explicit queue names in logs

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ var util = require('util');
 var EventEmitter = require('events');
 
 var RueQueue = function (params) {
-  EventEmitter.call(this);
-  this.setMaxListeners(1);
-
   if (typeof params.maxsize !== 'number' || params.maxsize < 1) {
     throw "must set maxsize";
   } else if (typeof params.callback !== 'function') {
@@ -16,95 +13,89 @@ var RueQueue = function (params) {
     throw "must provide name";
   }
 
+  EventEmitter.call(this);
+  this.setMaxListeners(1);
+
+  console.log("creating RueQueue", params.name);
+
   this.name = params.name;
-  this.queue = [];
+  this._queue = [];
   this.maxsize = params.maxsize;
   // this._regrets = 0;
   this.callback = params.callback;
   this.retryWait = params.retryWait || 5000;
   this.verbose = params.verbose || false;
 
-  this.push = function (val) {
-    if (this.queue.length+1 > this.maxsize) {
-      // this._regrets += 1;
-      var maybe_regret = this.queue.pop(); // end of the array is the oldest data
-      if (typeof maybe_regret._regret !== 'undefined') {
-        this.queue.pop(); // get rid of the oldest data
-        maybe_regret._regret += 1;
-        this.queue.push(maybe_regret);
-      } else {
-        this.queue.push({_regret: 1});
-      }
+  this.log = function() {
+    var args = [];
+    for(var i=0; i<arguments.length; i++){
+      args.push(arguments[i]);
     }
-
-    this.queue.unshift(val);
-
-    if (this._drainRetryTimer !== null) {
-      // Do nothing. The timer will fire in the future and retry.
-    } else {
-      this.emit('drain');
-    }
-  };
-
-  this.length = function() {
-    return this.queue.length;
+    args.unshift("RueQueue(" + this.name + ")");
+    console.log.apply(null, args);
   }
+
+  this.push = function (val) {
+    if (this._queue.length+1 > this.maxsize) {
+
+      var maybe_regret = this._queue.pop(); // end of the array is the oldest data
+      if (typeof maybe_regret._regret !== 'undefined') {
+        this._queue.pop(); // get rid of the oldest data
+        maybe_regret._regret += 1;
+        this._queue.push(maybe_regret);
+      } else {
+        this._queue.push({_regret: 1});
+      }
+
+    }
+
+    this._queue.unshift(val);
+    this.emit('drain');
+  };
 
   this.on('error', function() {
     this.resetDrainRetryTimer();
   });
 
   this.on('success', function() {
-    this.queue.pop();
+    this._queue.pop();
     this.emit('drain');
   });
 
   var me = this;
   this.success = function(){
-    if (me.verbose) {
-      console.log("callback invoked success");
-    }
     me.emit('success');
   };
 
   this.error = function(){
-    if (me.verbose) {
-      console.log("callback invoked error");
-    }
     me.emit('error');
   };
 
-  this._drainRunning = false;
   this.on('drain', function(){
-    if (this.queue.length === 0) {
-      // We stop trying to process messages
-      return;
+    if (this._queue.length === 0) {
+      return; // We stop trying to process messages until more are enqueued
     }
 
-    var entry = this.queue[this.queue.length-1]; // peek
+    var entry = this._queue[this._queue.length-1]; // peek
     try {
       this.callback(entry,this);
     } catch(e) {
-      if (this.verbose) {
-        console.log("RueQueue(", this.name, ") callback died with: ", e, this.queue);
-      } else {
-        console.log("RueQueue(", this.name, ") callback died with: ", e);
-      }
+      this.log("RueQueue(", this.name, ") callback died with: ", e);
       this.resetDrainRetryTimer();
     }
   });
 
   this._drainRetryTimer = null;
   this.resetDrainRetryTimer = function() {
-    if (typeof this._drainRetryTimer !== 'undefined') {
-      clearTimeout(this._drainRetryTimer);
-    }
+    clearTimeout(this._drainRetryTimer);
 
     var me = this;
     this._drainRetryTimer = setTimeout(function() {
+      // me.log("resetDrainRetryTimer calling 'drain'");
       me.emit('drain');
     }, this.retryWait);
   };
+
 };
 
 util.inherits(RueQueue, EventEmitter);
